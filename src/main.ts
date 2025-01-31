bd553d8af1b01d92e41d8c2676d6813a985d87c5
Djyauhen <fursaevgenij@gmail.com>
2025-01-31 14:16:04 +0300

    last change

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { CorsMiddleware } from "./middleware/cors.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   // app.use(new CorsMiddleware().use);
 
